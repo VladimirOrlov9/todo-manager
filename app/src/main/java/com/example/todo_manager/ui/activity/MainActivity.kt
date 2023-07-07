cbe047ff3e001d22dab7ac1e5a6b0ea654034bdd
VladimirOrlov9 <rallyfank@gmail.com>
2023-07-07 15:30:50 +0300

    Added navigation graph.

--- app/src/main/java/com/example/todo_manager/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/todo_manager/ui/activity/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.todo_manager.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.WindowCompat
 import com.example.todo_manager.R
+import com.example.todo_manager.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, true)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
